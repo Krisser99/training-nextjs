6ad2f8d90ff718e6217a3ad2f7a38948eaa27b28
namkt <namkt@gcosoftware.vn>
2022-07-02 18:02:14 +0700

    test -1

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,37 +1,45 @@
-import { GetStaticProps } from 'next'
-import Link from 'next/link'
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
-import { User } from '../../interfaces'
-import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
-import List from '../../components/List'
+import Layout from '../../components/Layout';
 
 type Props = {
-  items: User[]
-}
+    posts: any[];
+};
 
-const WithStaticProps = ({ items }: Props) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
-    <h1>Users List</h1>
-    <p>
-      Example fetching data from inside <code>getStaticProps()</code>.
-    </p>
-    <p>You are currently on: /users</p>
-    <List items={items} />
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const WithStaticProps = ({ posts }: Props) => {
+  console.log(posts)
+    return (
+        <Layout title="Users List | Next.js + TypeScript Example">
+            <h1>Users List</h1>
+            <p>
+                Example fetching data from inside <code>getStaticProps()</code>.
+            </p>
+            <p>You are currently on: /users</p>
+
+            <p>
+                <Link href="/">
+                    <a>Go home</a>
+                </Link>
+            </p>
+        </Layout>
+    );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const items: User[] = sampleUserData
-  return { props: { items } }
-}
+    // Example for including static props in a Next.js function component page.
+    // Don't forget to include the respective types for any props passed into
+    // the component.
+    const res = await fetch(
+        'https://api.gcosoftware.vn/wp-json/wp/v2/posts?search=web&per_page=5&page=1'
+    );
+    const posts = await res.json();
 
-export default WithStaticProps
+    return {
+        props: {
+            posts,
+        },
+    };
+};
+
+export default WithStaticProps;
